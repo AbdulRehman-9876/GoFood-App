cba0aa531d95819ea9848adeb0504605cc9e7922
AbdulRehman-9876 <abdulrehman9876@gmail.com>
2025-08-22 15:50:29 +0500

    Added Food Apis in frontend

--- src/Apis/authApi.js
+++ src/Apis/authApi.js
@@ -1,9 +1,10 @@
 // register, login, logout, token handling
 import axios from "axios";
+const URL = process.env.REACT_APP_URL;
 
 export const registerUser = async (userData) => {
   try {
-    await axios.post("http://localhost:5000/api/register", userData);
+    await axios.post(`${URL}/api/register`, userData);
   } catch (err) {
     console.log(`Error in fetching signup api ${err}`);
   }
@@ -11,10 +12,7 @@ export const registerUser = async (userData) => {
 
 export const loginUser = async (data) => {
   try {
-    const response = await axios.post(
-      "http://localhost:5000/api/login",
-      data
-    );
+    const response = await axios.post(`${URL}/api/login`, data);
 
     localStorage.setItem("authToken", response.data.authToken); //Setting Session Token
     localStorage.setItem("userData", JSON.stringify(response.data.userData)); //Setting Session User Data
--- src/Apis/foodApi.js
+++ src/Apis/foodApi.js
@@ -1,1 +1,25 @@
 // food items, categories, search
+import axios from "axios";
+const URL = process.env.REACT_APP_URL;
+
+const addFood = async (data) => {
+    try{
+        const response = await axios.post(`${URL}/food/addFood`, data)
+        return response.data;
+    } catch(err){
+        console.log(err)
+    }
+}
+const getFood = async () => {
+    try{
+        const response = await axios.get(`${URL}/food/getFood`)
+        return response.data;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = {
+    addFood,
+    getFood
+}
