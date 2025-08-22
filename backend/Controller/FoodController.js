d2f448c4fdb8995b56a9c8227e1bab2702d453c4
AbdulRehman-9876 <abdulrehman9876@gmail.com>
2025-08-22 15:42:00 +0500

    Added Get foods api

--- backend/Controller/FoodController.js
+++ backend/Controller/FoodController.js
@@ -1,23 +1,33 @@
 const Food = require("../models/Food");
 
-const addFood = async(req, res) => {
-    try{
-        const {name, category, image, description, price } = req.body;
-        const newFood = new Food({
-             name: name,
-             category: category,
-             image: image, 
-             description: description,
-             price: price,
-        })
-        newFood.save();
-        res.status(200).json({ message: "Food Successfully Added" });
-    }catch(err){
-        console.log(err)
-        res.status(400);
-    }
-}
+const addFood = async (req, res) => {
+  try {
+    const { name, category, image, description, price } = req.body;
+    const newFood = new Food({
+      name: name,
+      category: category,
+      image: image,
+      description: description,
+      price: price,
+    });
+    await newFood.save();
+    res.status(200).json({ message: "Food Successfully Added" });
+  } catch (err) {
+    console.log(err);
+    res.status(400);
+  }
+};
 
+const getFood = async (req, res) => {
+  try {
+    const food = await Food.find();
+    res.status(200).json(food);
+  } catch (err) {
+    console.log(err);
+    res.status(400);
+  }
+};
 module.exports = {
-    addFood
-}
+  addFood,
+  getFood,
+};
--- backend/Routes/Food.js
+++ backend/Routes/Food.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { addFood} = require("../Controller/FoodController");
+const { addFood, getFood} = require("../Controller/FoodController");
 
 router.post("/addFood", addFood);
+router.get("/getFood",getFood)
 
 module.exports = router;
